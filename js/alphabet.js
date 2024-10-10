12620af0e265c1d6fd356ac94d2df3cc1e0da0e0
MD-ABUSUFIAN <abusufian.programmer@gmail.com>
2024-10-10 06:27:36 +0600

    security login page function successfuly

--- js/alphabet.js
+++ js/alphabet.js
@@ -1,6 +1,24 @@
+const names=document.getElementById('name');
+const password=document.getElementById('password');
+const message=document.getElementById('message')
+
+
+let userName="";
+const goGame= function(){
+    if(password.value=="MERN-2404"){
+        userName=names.value;
+        names.value="";
+        password.value="";
+        shows('playHome','visually-hidden')
+        hidden('security','visually-hidden')
+    }
+    else{
+        message.innerText="your password is wrong plz type MERN-2404"
+        alert("WRONG PASSWORD PLZ TYPE : MERN-2404")
+    }
+}
 
 const playGame=function(){
-    // hiddenShows('visually-hidden','playHome')
     shows('playHome','visually-hidden')
     hidden('security','visually-hidden')
 }
