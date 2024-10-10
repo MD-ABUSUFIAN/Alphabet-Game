4f4c4c2618720d1cf62693345648821e586ce45d
MD-ABUSUFIAN <abusufian.programmer@gmail.com>
2024-10-10 06:00:45 +0600

    alphabet game all page create ok

---
+++ js/alphabet.js
@@ -0,0 +1,9 @@
+
+const playGame=function(){
+    // hiddenShows('visually-hidden','playHome')
+    shows('playHome','visually-hidden')
+    hidden('security','visually-hidden')
+}
+const againGame=function(){
+    console.log("play again button is working");
+}
---
+++ js/utility.js
@@ -0,0 +1,7 @@
+const hidden=function(sectionId,className){
+document.getElementById(sectionId).classList.add(className);
+}
+const shows =function(sectionId,className){
+document.getElementById(sectionId).classList.remove(className)
+}
+
